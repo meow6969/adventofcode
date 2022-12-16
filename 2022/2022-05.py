2b2be4568cd82c3ae26a56fac22e6a9e3998c25f
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-12-16 10:45:10 -0800

    Update 2022-05.py

--- 2022/2022-05.py
+++ 2022/2022-05.py
@@ -1,23 +1,16 @@
 data = open("input.txt").read().split("\n")
 
 
-
 for i, line in enumerate(data):
     if line[1].isdigit():
         counting_y = i
         break
-
-# print(counting_y)
-
-# for i in range(counting_y + 2):
-#     print(i)
 
 mult = 4
 pointer = 1
 counting_x = 0
 meowing = True
 while meowing:
-    # print(data[counting_y][pointer])
     try:
         if data[counting_y][pointer].isdigit():
             counting_x += 1 
@@ -26,30 +19,22 @@ while meowing:
     except IndexError:
         meowing = False
     pointer += mult
-# print(counting_x)
-
-# for y in range(counting_y, -1, -1):
-#     line = data[y]
-#     for x in range(counting_x):
 
 crates = []
 
-print(data)
-
+mult = 5
+offset = 0
 for x in range(counting_x):
     stack = []
+    if x == 0:
+        val = 1 
+    else:
+        val = x * mult - offset
+        offset += 1
     for y in range(counting_y, -1, -1):
-        
-        print(y)
-       
-        if x == 0:
-            val = 1 
-        else:
-            val = x * mult
-        print(x * mult)
-        if data[y][val].isdigit():
-            stack.append(data[y][x * mult])
+        if data[y][val].isalpha():
+            stack.append(data[y][val])
     crates.append(stack)
 print(crates)
     
-    
+# EVERYTHING ABOVE THIS CREATES A LIST    
